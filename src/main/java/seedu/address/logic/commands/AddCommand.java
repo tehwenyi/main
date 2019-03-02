f173dc88e9f8d01df5df89e53d93dcb9ed11ebe9
yunjun199321 <yunjun199321@gmail.com>
2019-03-02 21:44:34 +0800

    formats Application.java

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -5,15 +5,16 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_CATEGORY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_COST;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
+
+//import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+//import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 
 /**
  * Adds a person to the address book.
@@ -23,26 +24,8 @@ public class AddCommand extends Command {
     public static final String COMMAND_WORD = "add";
     public static final String COMMAND_ALIAS = "a";
 
-/*
-original code
-
-public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to the address book. "
-+ "Parameters: "
-+ PREFIX_NAME + "NAME "
-+ PREFIX_PHONE + "PHONE "
-+ PREFIX_EMAIL + "EMAIL "
-+ PREFIX_ADDRESS + "ADDRESS "
-+ "[" + PREFIX_TAG + "TAG]...\n"
-+ "Example: " + COMMAND_WORD + " "
-+ PREFIX_NAME + "John Doe "
-+ PREFIX_PHONE + "98765432 "
-+ PREFIX_EMAIL + "johnd@example.com "
-+ PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-+ PREFIX_TAG + "friends "
-+ PREFIX_TAG + "owesMoney";
-*/
-
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds an expense to the ePiggy. "
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Adds an expense to the ePiggy. "
             + "Parameters: "
             + PREFIX_NAME + "EXPENSE NAME "
             + PREFIX_COST + "COST "
@@ -51,11 +34,11 @@ public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a person to th
             + "[" + PREFIX_ADDRESS + "ADDRESS] "
             + "[" + PREFIX_TAG + "TAG]...\n"
             + "Example: " + COMMAND_WORD + " "
-            + PREFIX_NAME + "Chicken Rice Set"
+            + PREFIX_NAME + "Chicken Rice Set "
             + PREFIX_COST + "15 "
-            + PREFIX_CATEGORY + "Food"
+            + PREFIX_CATEGORY + "Food "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
-            + PREFIX_DATE + "2019-02-21"
+            + PREFIX_DATE + "2019-02-21 "
             + PREFIX_TAG + "Lunch";
 
     public static final String MESSAGE_SUCCESS = "New expense added: %1$s";

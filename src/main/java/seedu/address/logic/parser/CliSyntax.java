3cc7b970b8d1e3d4a64e790aeae8069328a82322
yunjun199321 <yunjun199321@gmail.com>
2019-03-02 16:24:27 +0800

    Adds new prefix in CliSyntax.java

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -5,11 +5,16 @@ package seedu.address.logic.parser;
  */
 public class CliSyntax {
 
+    // TODO: remove unused prefix.
     /* Prefix definitions */
     public static final Prefix PREFIX_NAME = new Prefix("n/");
     public static final Prefix PREFIX_PHONE = new Prefix("p/");
     public static final Prefix PREFIX_EMAIL = new Prefix("e/");
     public static final Prefix PREFIX_ADDRESS = new Prefix("a/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
+
+    public static final Prefix PREFIX_COST = new Prefix("$/");
+    public static final Prefix PREFIX_CATEGORY = new Prefix("c/");
+    public static final Prefix PREFIX_DATE = new Prefix("d/");
 
 }

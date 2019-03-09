7187a7819041ae61848d740ad9588bf54a29efea
yunjun199321 <yunjun199321@gmail.com>
2019-03-09 16:03:01 +0800

    Adds SummaryCommand class

---
+++ src/main/java/seedu/address/logic/commands/SummaryCommand.java
@@ -0,0 +1,24 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.model.Model;
+
+/**
+ * Shows summary to the user.
+ */
+public class SummaryCommand extends Command {
+    public static final String COMMAND_WORD = "summary";
+    public static final String COMMAND_ALIAS = "y";
+    public static final String COMMAND_USAGE = COMMAND_WORD
+            + ": Shows summary to the user.";
+
+    public static final String MESSAGE_SUCCESS = "Showed summary.";
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) {
+        requireNonNull(model);
+        return new CommandResult(MESSAGE_SUCCESS);
+    }
+}

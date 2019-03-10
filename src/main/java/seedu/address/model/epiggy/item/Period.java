101f8b8b467236299f1f587321b51c6440bb1ed2
tehwenyi <teh.wenyi22@gmail.com>
2019-03-10 18:51:28 +0800

    add validationregex and messageconstraints in Period to check if period is valid

--- src/main/java/seedu/address/model/epiggy/item/Period.java
+++ src/main/java/seedu/address/model/epiggy/item/Period.java
@@ -5,6 +5,9 @@ package seedu.address.model.epiggy.item;
  * Guarantees: immutable
  */
 public class Period {
+    public static final String MESSAGE_CONSTRAINTS =
+            "Time period is in terms of weeks and should only contain whole numbers of at least value 1.";
+    public static final String VALIDATION_REGEX = "\\d+";
     private final int timePeriod;
 
     public Period(int timePeriod) {
@@ -13,5 +16,12 @@ public class Period {
 
     public int getTimePeriod() {
         return timePeriod;
+    }
+
+    /**
+     * Returns true if a given string is a valid time period.
+     */
+    public static boolean isValidPeriod(String test) {
+        return test.matches(VALIDATION_REGEX);
     }
 }

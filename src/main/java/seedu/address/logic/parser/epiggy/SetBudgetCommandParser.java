360effa5c131f7c7fa7323945508b56b0d7c495a
tehwenyi <teh.wenyi22@gmail.com>
2019-03-10 18:20:01 +0800

    add a parser for setbudgetcommand

---
+++ src/main/java/seedu/address/logic/parser/epiggy/SetBudgetCommandParser.java
@@ -0,0 +1,54 @@
+package seedu.address.logic.parser.epiggy;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_COST;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PERIOD;
+
+import java.util.stream.Stream;
+
+import seedu.address.logic.commands.epiggy.SetBudgetCommand;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.ParserUtil;
+import seedu.address.logic.parser.Prefix;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.epiggy.Budget;
+import seedu.address.model.epiggy.item.Period;
+import seedu.address.model.epiggy.item.Price;
+
+/**
+ * Parses input arguments and creates a new AddCommand object
+ */
+public class SetBudgetCommandParser implements Parser<SetBudgetCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the SetBudgetCommand
+     * and returns an SetBudgetCommand object for execution.
+     */
+    public SetBudgetCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_COST, PREFIX_PERIOD);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_COST, PREFIX_PERIOD)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SetBudgetCommand.MESSAGE_USAGE));
+        }
+
+        Price price = ParserUtil.parseCost(argMultimap.getValue(PREFIX_COST).get());
+        Period period = ParserUtil.parsePeriod(argMultimap.getValue(PREFIX_PERIOD).get());
+
+        Budget budget = new Budget(price, period);
+
+        return new SetBudgetCommand(budget);
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
+}

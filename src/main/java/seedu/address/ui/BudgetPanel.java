e6dd7150ccbe5b73b7f425c71f272c17543a3e11
tehwenyi <teh.wenyi22@gmail.com>
2019-03-16 17:07:52 +0800

    Updated BudgetPanel which prints Hello World

--- src/main/java/seedu/address/ui/BudgetPanel.java
+++ src/main/java/seedu/address/ui/BudgetPanel.java
@@ -2,21 +2,12 @@ package seedu.address.ui;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextFlow;
-import seedu.address.commons.core.LogsCenter;
-import seedu.address.model.epiggy.Budget;
-import seedu.address.model.epiggy.Expense;
 
 /**
  * Panel containing the budget status.
@@ -35,14 +26,6 @@ public class BudgetPanel extends UiPart<Region> {
         string.add("Hello");
         string.add("World");
         budgetView.setItems(string);
-        Text text1 = new Text("Budget");
-        // text1.setFill(Color.RED);
-        text1.setFont(Font.font("Helvetica", FontPosture.ITALIC, 40));
-        Text text2 = new Text(" trytry");
-        // text2.setFill(Color.BLUE);
-        text2.setFont(Font.font("Helvetica", FontWeight.BOLD, 10));
-        TextFlow textFlow = new TextFlow(text1, text2);
-
     }
 
 }

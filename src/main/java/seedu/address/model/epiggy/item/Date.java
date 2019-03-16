1fc62aeffc8f688d8516c01358f78b1bf6145733
tehwenyi <teh.wenyi22@gmail.com>
2019-03-16 12:05:18 +0800

    add addDays method in Date class to calculate the Date after adding a certain amount of days

--- src/main/java/seedu/address/model/epiggy/item/Date.java
+++ src/main/java/seedu/address/model/epiggy/item/Date.java
@@ -1,6 +1,6 @@
 package seedu.address.model.epiggy.item;
 
-import java.util.Calendar;
+import java.util.HashMap;
 
 /**
  * Represents a Date in ePiggy.
@@ -16,14 +16,97 @@ public class Date {
             + "[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|"
             + "[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|"
             + "2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$";
+    private static final HashMap<Integer, Integer> daysInMonth;
     private final int day;
     private final int month;
     private final int year;
+
+    static {
+        daysInMonth = new HashMap<Integer, Integer>();
+        daysInMonth.put(1, 31);
+        daysInMonth.put(2, 28);
+        daysInMonth.put(3, 31);
+        daysInMonth.put(4, 30);
+        daysInMonth.put(5, 31);
+        daysInMonth.put(6, 30);
+        daysInMonth.put(7, 31);
+        daysInMonth.put(8, 31);
+        daysInMonth.put(9, 30);
+        daysInMonth.put(10, 31);
+        daysInMonth.put(11, 30);
+        daysInMonth.put(12, 31);
+    }
 
     public Date(String date) {
         this.day = Integer.valueOf(date.substring(0, 2));
         this.month = Integer.valueOf(date.substring(3, 5));
         this.year = Integer.valueOf(date.substring(6, 10));
+    }
+
+    public Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    /**
+     * Adds the number of days required to add to start day and calls the recursive function
+     * @param addedDays
+     * @return Date after adding the days
+     */
+    public Date addDays (int addedDays) {
+        return addDays(this.day + addedDays, this.month, this.year);
+    }
+
+    /**
+     * Recursive function which adds the number of days to the date
+     * @param days which is the sum of days to add and start day
+     * @param month of the start date
+     * @param year of the start date
+     * @return Date after adding the days
+     */
+    public Date addDays(int days, int month, int year) {
+        if (days > 0 && days <= getNoOfDaysInMonth(month, year)) {
+            return new Date(days, month, year);
+        } else if (days > 0) {
+            days = days - getNoOfDaysInMonth(month, year);
+            month = month + 1;
+            if (month > 12) {
+                month = 1;
+                year = year + 1;
+            }
+        } else {
+            //throw exception
+        }
+        return addDays(days, month, year);
+    }
+
+    private int getNoOfDaysInMonth(int month, int year) {
+        if (month == 2 && isLeap(year)) {
+            return daysInMonth.get(month) + 1;
+        }
+        return daysInMonth.get(month);
+    }
+
+    /**
+     * Check if given year is a leap year
+     * @param year
+     * @return true if year is leap year
+     */
+    public boolean isLeap(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -56,18 +139,6 @@ public class Date {
      */
     public static boolean isValidDate(String test) {
         return test.matches(VALIDATION_REGEX);
-    }
-
-    /**
-     * Check if given year is a leap year
-     * @param year
-     * @return true if year is leap year
-     */
-    public boolean isLeap(int year) {
-        // uses system library to check
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, year);
-        return cal.getActualMaximum(Calendar.DAY_OF_YEAR) > 365;
     }
 
     @Override

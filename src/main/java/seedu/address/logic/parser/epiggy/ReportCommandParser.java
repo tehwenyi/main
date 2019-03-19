17e48dfc8f152ba857fbe41ac729d03f20c493be
yunjun199321 <yunjun199321@gmail.com>
2019-03-19 21:56:34 +0800

    Adds report parser, GUI button

---
+++ src/main/java/seedu/address/logic/parser/epiggy/ReportCommandParser.java
@@ -0,0 +1,51 @@
+package seedu.address.logic.parser.epiggy;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PERIOD;
+
+import java.util.stream.Stream;
+
+import seedu.address.logic.commands.epiggy.ReportCommand;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.Prefix;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and display report.
+ */
+public class ReportCommandParser implements Parser<ReportCommand> {
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the ReportCommand
+     * and returns an ReportCommand object for execution.
+     */
+    @Override
+    public ReportCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_PERIOD);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_PERIOD)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ReportCommand.MESSAGE_USAGE));
+        }
+
+        if (argMultimap.getValue(PREFIX_PERIOD).isPresent()) {
+            String type = argMultimap.getValue(PREFIX_PERIOD).get();
+            return new ReportCommand(type.toUpperCase());
+        } else {
+            return new ReportCommand();
+        }
+
+
+    }
+}

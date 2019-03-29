c5bd16a57a56b2f2d2fd8b929c433b0c88e2b7c3
tehwenyi <teh.wenyi22@gmail.com>
2019-03-29 14:46:41 +0800

    Add BudgetBuilder class for testing purposes

--- src/test/java/seedu/address/testutil/epiggy/BudgetBuilder.java
+++ src/test/java/seedu/address/testutil/epiggy/BudgetBuilder.java
@@ -1,93 +1,65 @@
-package seedu.address.testutil;
+package seedu.address.testutil.epiggy;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Date;
 
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
-import seedu.address.model.tag.Tag;
-import seedu.address.model.util.SampleDataUtil;
+import seedu.address.model.epiggy.Budget;
+import seedu.address.model.epiggy.item.Period;
+import seedu.address.model.epiggy.item.Price;
 
 /**
- * A utility class to help with building Person objects.
+ * A utility class to help with building Budget objects.
  */
-public class PersonBuilder {
+public class BudgetBuilder {
 
-    public static final String DEFAULT_NAME = "Alice Pauline";
-    public static final String DEFAULT_PHONE = "85355255";
-    public static final String DEFAULT_EMAIL = "alice@gmail.com";
-    public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
+    public static final String DEFAULT_AMOUNT = "500";
+    public static final String DEFAULT_PERIOD = "25";
+    public static final String DEFAULT_STARTDATE = "03/02/2019";
 
-    private Name name;
-    private Phone phone;
-    private Email email;
-    private Address address;
-    private Set<Tag> tags;
+    private Price amount;
+    private Period days;
+    private Date startDate;
 
-    public PersonBuilder() {
-        name = new Name(DEFAULT_NAME);
-        phone = new Phone(DEFAULT_PHONE);
-        email = new Email(DEFAULT_EMAIL);
-        address = new Address(DEFAULT_ADDRESS);
-        tags = new HashSet<>();
+    public BudgetBuilder() {
+        amount = new Price(DEFAULT_AMOUNT);
+        days = new Period(DEFAULT_PERIOD);
+        startDate = new Date(DEFAULT_STARTDATE);
     }
 
     /**
-     * Initializes the PersonBuilder with the data of {@code personToCopy}.
+     * Initializes the BudgetBuilder with the data of {@code budgetToCopy}.
      */
-    public PersonBuilder(Person personToCopy) {
-        name = personToCopy.getName();
-        phone = personToCopy.getPhone();
-        email = personToCopy.getEmail();
-        address = personToCopy.getAddress();
-        tags = new HashSet<>(personToCopy.getTags());
+    public BudgetBuilder(Budget budgetToCopy) {
+        amount = budgetToCopy.getPrice();
+        days = budgetToCopy.getPeriod();
+        startDate = budgetToCopy.getStartDate();
     }
 
     /**
-     * Sets the {@code Name} of the {@code Person} that we are building.
+     * Sets the {@code Price} of the {@code Budget} that we are building.
      */
-    public PersonBuilder withName(String name) {
-        this.name = new Name(name);
+    public BudgetBuilder withAmount(String amount) {
+        this.amount = new Price(amount);
         return this;
     }
 
     /**
-     * Parses the {@code tags} into a {@code Set<Tag>} and set it to the {@code Person} that we are building.
+     * Sets the {@code Period} of the {@code Budget} that we are building.
      */
-    public PersonBuilder withTags(String ... tags) {
-        this.tags = SampleDataUtil.getTagSet(tags);
+    public BudgetBuilder withAddress(String days) {
+        this.days = new Period(days);
         return this;
     }
 
     /**
-     * Sets the {@code Address} of the {@code Person} that we are building.
+     * Sets the {@code Date} of the {@code Budget} that we are building.
      */
-    public PersonBuilder withAddress(String address) {
-        this.address = new Address(address);
+    public BudgetBuilder withPhone(String startDate) {
+        this.startDate = new Date(startDate);
         return this;
     }
 
-    /**
-     * Sets the {@code Phone} of the {@code Person} that we are building.
-     */
-    public PersonBuilder withPhone(String phone) {
-        this.phone = new Phone(phone);
-        return this;
-    }
-
-    /**
-     * Sets the {@code Email} of the {@code Person} that we are building.
-     */
-    public PersonBuilder withEmail(String email) {
-        this.email = new Email(email);
-        return this;
-    }
-
-    public Person build() {
-        return new Person(name, phone, email, address, tags);
+    public Budget build() {
+        return new Budget(amount, days, startDate);
     }
 
 }

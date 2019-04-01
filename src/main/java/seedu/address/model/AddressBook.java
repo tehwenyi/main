0482f95a7ac4700a0ed2594aa68d6f46499f4acb
Kevin <kevinchanxy@gmail.com>
2019-04-02 00:00:30 +0800

    updated delete expense for ui and storage

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -194,6 +194,13 @@ public class AddressBook implements ReadOnlyEPiggy {
      */
     public void deleteExpense(Expense toDelete) {
         expenses.remove(toDelete);
+        Savings s = savings.get();
+        if (toDelete instanceof Allowance) {
+            s.deductSavings(toDelete.getItem().getCost().getAmount());
+        } else {
+            s.addSavings(toDelete.getItem().getCost().getAmount());
+        }
+        this.savings.setValue(new Savings(s));
         indicateModified();
     }
 

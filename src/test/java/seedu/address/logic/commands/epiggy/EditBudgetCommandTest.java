aa7ebc603453f0470442be42922fdd4d0664f105
tehwenyi <teh.wenyi22@gmail.com>
2019-04-02 19:19:38 +0800

    Add some new test cases for editBudgetCommand

--- src/test/java/seedu/address/logic/commands/epiggy/EditBudgetCommandTest.java
+++ src/test/java/seedu/address/logic/commands/epiggy/EditBudgetCommandTest.java
@@ -1,30 +1,38 @@
 package seedu.address.logic.commands.epiggy;
 
-import static java.util.Objects.requireNonNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_ONE;
+import static seedu.address.logic.commands.CommandTestUtil.DESC_TWO;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_AMOUNT_ONE;
+import static seedu.address.logic.commands.epiggy.EditBudgetCommand.createEditedBudget;
+import static seedu.address.testutil.TypicalBudgets.getTypicalEPiggy;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.function.Predicate;
-import javafx.beans.property.ReadOnlyProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import org.junit.Assert;
+
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import javafx.beans.property.ReadOnlyProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import seedu.address.commons.core.GuiSettings;
-import seedu.address.commons.core.Messages;
-import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.epiggy.EditBudgetCommand.EditBudgetDetails;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.EPiggy;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.ReadOnlyEPiggy;
@@ -35,25 +43,15 @@ import seedu.address.model.epiggy.Budget;
 import seedu.address.model.epiggy.Expense;
 import seedu.address.model.epiggy.Goal;
 import seedu.address.model.epiggy.Savings;
+import seedu.address.model.epiggy.item.Cost;
+import seedu.address.model.epiggy.item.Period;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.epiggy.BudgetBuilder;
 import seedu.address.testutil.epiggy.EditBudgetDetailsBuilder;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
-
-import static seedu.address.logic.commands.CommandTestUtil.DESC_ONE;
-import static seedu.address.logic.commands.CommandTestUtil.DESC_TWO;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_AMOUNT_ONE;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.epiggy.EditBudgetCommand.createEditedBudget;
-import static seedu.address.testutil.TypicalBudgets.getTypicalEPiggy;
-
 /**
- * Contains integration tests (interaction with the Model, UndoCommand and RedoCommand) and unit tests for EditBudgetCommand.
+ * Contains integration tests (interaction with the Model, UndoCommand and RedoCommand)
+ * and unit tests for EditBudgetCommand.
  */
 @Ignore
 public class EditBudgetCommandTest {
@@ -68,7 +66,7 @@ public class EditBudgetCommandTest {
     private Date todaysDate = new Date();
 
     @Test
-    public void execute_allFieldsSpecifiedUnfilteredList_success() throws Exception {
+    public void execute_allFieldsSpecified_success() throws Exception {
         ModelStubWithOneCurrentBudget modelStub = new ModelStubWithOneCurrentBudget();
 
         EditBudgetDetails details = DESC_ONE;
@@ -82,81 +80,64 @@ public class EditBudgetCommandTest {
         assertEquals(EMPTY_COMMAND_HISTORY, commandHistory);
     }
 
-//    @Test
-//    public void execute_someFieldsSpecifiedUnfilteredList_success() {
-//        Index indexLastBudget = Index.fromOneBased(model.getFilteredBudgetList().size());
-//        Budget lastBudget = model.getFilteredBudgetList().get(indexLastBudget.getZeroBased());
-//
-//        BudgetBuilder personInList = new BudgetBuilder(lastBudget);
-//        Budget editedBudget = personInList.withAmount(VALID_AMOUNT_ONE).withPeriod(VALID_PERIOD_ONE)
-//                .withDate(VALID_DATE_HUSBAND).build();
-//
-//        EditBudgetDetails details = new EditBudgetDetailsBuilder().withAmount(VALID_AMOUNT_ONE)
-//                .withPeriod(VALID_PERIOD_ONE).withDate(VALID_DATE_HUSBAND).build();
-//        EditBudgetCommand editBudgetCommand = new EditBudgetCommand(indexLastBudget, details);
-//
-//        String expectedMessage = String.format(EditBudgetCommand.MESSAGE_EDIT_PERSON_SUCCESS, editedBudget);
-//
-//        Model expectedModel = new ModelManager(new EPiggy(model.getEPiggy()), new UserPrefs());
-//        expectedModel.setBudget(lastBudget, editedBudget);
-//        expectedModel.commitEPiggy();
-//
-//        assertCommandSuccess(editBudgetCommand, model, commandHistory, expectedMessage, expectedModel);
-//    }
-//
-//    @Test
-//    public void execute_noFieldSpecifiedUnfilteredList_success() {
-//        EditBudgetCommand editBudgetCommand = new EditBudgetCommand(INDEX_FIRST_BUDGET, new EditBudgetDetails());
-//        Budget editedBudget = model.getFilteredBudgetList().get(INDEX_FIRST_BUDGET.getZeroBased());
-//
-//        String expectedMessage = String.format(EditBudgetCommand.MESSAGE_EDIT_PERSON_SUCCESS, editedBudget);
-//
-//        Model expectedModel = new ModelManager(new EPiggy(model.getEPiggy()), new UserPrefs());
-//        expectedModel.commitEPiggy();
-//
-//        assertCommandSuccess(editBudgetCommand, model, commandHistory, expectedMessage, expectedModel);
-//    }
-//
-//    @Test
-//    public void execute_filteredList_success() {
-//        showBudgetAtIndex(model, INDEX_FIRST_BUDGET);
-//
-//        Budget personInFilteredList = model.getFilteredBudgetList().get(INDEX_FIRST_BUDGET.getZeroBased());
-//        Budget editedBudget = new BudgetBuilder(personInFilteredList).withAmount(VALID_AMOUNT_ONE).build();
-//        EditBudgetCommand editBudgetCommand = new EditBudgetCommand(INDEX_FIRST_BUDGET,
-//                new EditBudgetDetailsBuilder().withAmount(VALID_AMOUNT_ONE).build());
-//
-//        String expectedMessage = String.format(EditBudgetCommand.MESSAGE_EDIT_PERSON_SUCCESS, editedBudget);
-//
-//        Model expectedModel = new ModelManager(new EPiggy(model.getEPiggy()), new UserPrefs());
-//        expectedModel.setBudget(model.getFilteredBudgetList().get(0), editedBudget);
-//        expectedModel.commitEPiggy();
-//
-//        assertCommandSuccess(editBudgetCommand, model, commandHistory, expectedMessage, expectedModel);
-//    }
-//
-//    @Ignore
-//    @Test
-//    public void execute_duplicateBudgetUnfilteredList_failure() {
-//        Budget firstBudget = model.getFilteredBudgetList().get(INDEX_FIRST_BUDGET.getZeroBased());
-//        EditBudgetDetails details = new EditBudgetDetailsBuilder(firstBudget).build();
-//        EditBudgetCommand editBudgetCommand = new EditBudgetCommand(INDEX_SECOND_BUDGET, details);
-//
-//        assertCommandFailure(editBudgetCommand, model, commandHistory, EditBudgetCommand.MESSAGE_DUPLICATE_PERSON);
-//    }
-//
-//    @Ignore
-//    @Test
-//    public void execute_duplicateBudgetFilteredList_failure() {
-//        showBudgetAtIndex(model, INDEX_FIRST_BUDGET);
-//
-//        // edit person in filtered list into a duplicate in address book
-//        Budget personInList = model.getEPiggy().getBudgetList().get(INDEX_SECOND_BUDGET.getZeroBased());
-//        EditBudgetCommand editBudgetCommand = new EditBudgetCommand(INDEX_FIRST_BUDGET,
-//                new EditBudgetDetailsBuilder(personInList).build());
-//
-//        assertCommandFailure(editBudgetCommand, model, commandHistory, EditBudgetCommand.MESSAGE_DUPLICATE_PERSON);
-//    }
+    @Test
+    public void execute_amountSpecified_success() throws Exception {
+        ModelStubWithOneCurrentBudget modelStub = new ModelStubWithOneCurrentBudget();
+
+        Budget budgetToEdit = new BudgetBuilder().build();
+        EditBudgetDetails details = new EditBudgetDetails();
+        details.setPeriod(budgetToEdit.getPeriod());
+        details.setStartDate(budgetToEdit.getStartDate());
+        details.setAmount(new Cost(200));
+
+        Budget editedBudget = createEditedBudget(budgetToEdit, details);
+        CommandResult commandResult = new EditBudgetCommand(details).execute(modelStub, commandHistory);
+
+        assertEquals(String.format(EditBudgetCommand.MESSAGE_EDIT_BUDGET_SUCCESS, editedBudget),
+                commandResult.getFeedbackToUser());
+        assertEquals(Arrays.asList(editedBudget), modelStub.budgets);
+        assertEquals(EMPTY_COMMAND_HISTORY, commandHistory);
+    }
+
+    @Test
+    public void execute_periodSpecified_success() throws Exception {
+        ModelStubWithOneCurrentBudget modelStub = new ModelStubWithOneCurrentBudget();
+
+        Budget budgetToEdit = new BudgetBuilder().build();
+        EditBudgetDetails details = new EditBudgetDetails();
+        details.setStartDate(budgetToEdit.getStartDate());
+        details.setAmount(budgetToEdit.getCost());
+        details.setPeriod(new Period(26));
+
+        Budget editedBudget = createEditedBudget(budgetToEdit, details);
+        CommandResult commandResult = new EditBudgetCommand(details).execute(modelStub, commandHistory);
+
+        assertEquals(String.format(EditBudgetCommand.MESSAGE_EDIT_BUDGET_SUCCESS, editedBudget),
+                commandResult.getFeedbackToUser());
+        assertEquals(Arrays.asList(editedBudget), modelStub.budgets);
+        assertEquals(EMPTY_COMMAND_HISTORY, commandHistory);
+    }
+
+    @Test
+    public void execute_dateSpecified_success() throws Exception {
+        ModelStubWithOneCurrentBudget modelStub = new ModelStubWithOneCurrentBudget();
+
+        Budget budgetToEdit = new BudgetBuilder().build();
+        EditBudgetDetails details = new EditBudgetDetails();
+        details.setAmount(budgetToEdit.getCost());
+        details.setPeriod(budgetToEdit.getPeriod());
+
+        Date newDate = new GregorianCalendar(2010, Calendar.FEBRUARY, 11).getTime();
+        details.setStartDate(newDate);
+
+        Budget editedBudget = createEditedBudget(budgetToEdit, details);
+        CommandResult commandResult = new EditBudgetCommand(details).execute(modelStub, commandHistory);
+
+        assertEquals(String.format(EditBudgetCommand.MESSAGE_EDIT_BUDGET_SUCCESS, editedBudget),
+                commandResult.getFeedbackToUser());
+        assertEquals(Arrays.asList(editedBudget), modelStub.budgets);
+        assertEquals(EMPTY_COMMAND_HISTORY, commandHistory);
+    }
 
     @Test
     public void execute_noCurrentBudget_failure() throws Exception {
@@ -169,93 +150,6 @@ public class EditBudgetCommandTest {
         thrown.expectMessage(EditBudgetCommand.MESSAGE_EDIT_BUDGET_FAIL);
         editBudgetCommand.execute(modelStub, commandHistory);
     }
-
-//    /**
-//     * Edit filtered list where index is larger than size of filtered list,
-//     * but smaller than size of address book
-//     */
-//    @Ignore
-//    @Test
-//    public void execute_invalidBudgetIndexFilteredList_failure() {
-//        showBudgetAtIndex(model, INDEX_FIRST_BUDGET);
-//        Index outOfBoundIndex = INDEX_SECOND_BUDGET;
-//        // ensures that outOfBoundIndex is still in bounds of address book list
-//        assertTrue(outOfBoundIndex.getZeroBased() < model.getEPiggy().getBudgetList().size());
-//
-//        EditBudgetCommand editBudgetCommand = new EditBudgetCommand(outOfBoundIndex,
-//                new EditBudgetDetailsBuilder().withAmount(VALID_AMOUNT_ONE).build());
-//
-//        assertCommandFailure(editBudgetCommand, model, commandHistory, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-//    }
-//
-//    @Ignore
-//    @Test
-//    public void executeUndoRedo_validIndexUnfilteredList_success() throws Exception {
-//        Budget editedBudget = new BudgetBuilder().build();
-//        Budget personToEdit = model.getFilteredBudgetList().get(INDEX_FIRST_BUDGET.getZeroBased());
-//        EditBudgetDetails details = new EditBudgetDetailsBuilder(editedBudget).build();
-//        EditBudgetCommand editBudgetCommand = new EditBudgetCommand(INDEX_FIRST_BUDGET, details);
-//        Model expectedModel = new ModelManager(new EPiggy(model.getEPiggy()), new UserPrefs());
-//        expectedModel.setBudget(personToEdit, editedBudget);
-//        expectedModel.commitEPiggy();
-//
-//        // edit -> first person edited
-//        editBudgetCommand.execute(model, commandHistory);
-//
-//        // undo -> reverts addressbook back to previous state and filtered person list to show all persons
-//        expectedModel.undoEPiggy();
-//        assertCommandSuccess(new UndoCommand(), model, commandHistory, UndoCommand.MESSAGE_SUCCESS, expectedModel);
-//
-//        // redo -> same first person edited again
-//        expectedModel.redoEPiggy();
-//        assertCommandSuccess(new RedoCommand(), model, commandHistory, RedoCommand.MESSAGE_SUCCESS, expectedModel);
-//    }
-//
-//    @Test
-//    public void executeUndoRedo_invalidIndexUnfilteredList_failure() {
-//        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredBudgetList().size() + 1);
-//        EditBudgetDetails details = new EditBudgetDetailsBuilder().withAmount(VALID_AMOUNT_ONE).build();
-//        EditBudgetCommand editBudgetCommand = new EditBudgetCommand(outOfBoundIndex, details);
-//
-//        // execution failed -> address book state not added into model
-//        assertCommandFailure(editBudgetCommand, model, commandHistory, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
-//
-//        // single address book state in model -> undoCommand and redoCommand fail
-//        assertCommandFailure(new UndoCommand(), model, commandHistory, UndoCommand.MESSAGE_FAILURE);
-//        assertCommandFailure(new RedoCommand(), model, commandHistory, RedoCommand.MESSAGE_FAILURE);
-//    }
-//
-//    /**
-//     * 1. Edits a {@code Budget} from a filtered list.
-//     * 2. Undo the edit.
-//     * 3. The unfiltered list should be shown now. Verify that the index of the previously edited person in the
-//     * unfiltered list is different from the index at the filtered list.
-//     * 4. Redo the edit. This ensures {@code RedoCommand} edits the person object regardless of indexing.
-//     */
-//    @Test
-//    public void executeUndoRedo_validIndexFilteredList_sameBudgetEdited() throws Exception {
-//        Budget editedBudget = new BudgetBuilder().build();
-//        EditBudgetDetails details = new EditBudgetDetailsBuilder(editedBudget).build();
-//        EditBudgetCommand editBudgetCommand = new EditBudgetCommand(INDEX_FIRST_BUDGET, details);
-//        Model expectedModel = new ModelManager(new EPiggy(model.getEPiggy()), new UserPrefs());
-//
-//        showBudgetAtIndex(model, INDEX_SECOND_BUDGET);
-//        Budget personToEdit = model.getFilteredBudgetList().get(INDEX_FIRST_BUDGET.getZeroBased());
-//        expectedModel.setBudget(personToEdit, editedBudget);
-//        expectedModel.commitEPiggy();
-//
-//        // edit -> edits second person in unfiltered person list / first person in filtered person list
-//        editBudgetCommand.execute(model, commandHistory);
-//
-//        // undo -> reverts addressbook back to previous state and filtered person list to show all persons
-//        expectedModel.undoEPiggy();
-//        assertCommandSuccess(new UndoCommand(), model, commandHistory, UndoCommand.MESSAGE_SUCCESS, expectedModel);
-//
-//        assertNotEquals(model.getFilteredBudgetList().get(INDEX_FIRST_BUDGET.getZeroBased()), personToEdit);
-//        // redo -> edits same second person in unfiltered person list
-//        expectedModel.redoEPiggy();
-//        assertCommandSuccess(new RedoCommand(), model, commandHistory, RedoCommand.MESSAGE_SUCCESS, expectedModel);
-//    }
 
     @Test
     public void equals() {

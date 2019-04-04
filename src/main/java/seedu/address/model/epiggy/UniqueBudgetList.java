8af90256cba3727cd6d5b43e5ea395c54795bbf6
tehwenyi <teh.wenyi22@gmail.com>
2019-04-04 21:06:54 +0800

    Finish UniqueBudgetListTest

--- src/main/java/seedu/address/model/epiggy/UniqueBudgetList.java
+++ src/main/java/seedu/address/model/epiggy/UniqueBudgetList.java
@@ -35,11 +35,8 @@ public class UniqueBudgetList implements Iterable<Budget> {
      * Only called when a new budget is added.
      * @param toAdd the budget to be added.
      */
-    public void addAtIndex(int index, Budget toAdd) throws IndexOutOfBoundsException {
-        requireAllNonNull(index, toAdd);
-        if (index > MAXIMUM_SIZE) {
-            throw new IndexOutOfBoundsException();
-        }
+    public void addAtIndex(int index, Budget toAdd) {
+        requireNonNull(toAdd);
         internalList.add(index, toAdd);
         limitSize();
     }
@@ -48,23 +45,10 @@ public class UniqueBudgetList implements Iterable<Budget> {
      * Replaces the budget at index {@code index} with budget {@code toSet}.
      * @param toSet the budget to be added.
      */
-    public void replaceAtIndex(int index, Budget toSet) throws IndexOutOfBoundsException {
+    public void replaceAtIndex(int index, Budget toSet) {
         requireNonNull(toSet);
-        if (index < 0 || index >= internalList.size()) {
-            throw new IndexOutOfBoundsException();
-        }
         internalList.set(index, toSet);
         limitSize();
-    }
-
-    /**
-     * Gets the last budget on the internal list, which is the previous budget.
-     * There must be at least one budget in {@code internalList}
-     * @return the latest budget in {@code internalList}.
-     */
-    public Budget getLatestBudget() {
-        requireNonNull(internalList);
-        return internalList.get(0);
     }
 
     /**
@@ -73,7 +57,6 @@ public class UniqueBudgetList implements Iterable<Budget> {
      * @return the corresponding budget in {@code internalList}.
      */
     public Budget getBudgetAtIndex(int index) {
-        requireNonNull(internalList);
         return internalList.get(index);
     }
 
@@ -82,7 +65,7 @@ public class UniqueBudgetList implements Iterable<Budget> {
      * @return the index of the budget or -1 if the expense date is not in any of the budgets.
      */
     public int getBudgetIndexBasedOnDate(Date date) {
-        requireNonNull(internalList);
+        requireAllNonNull(internalList, date);
 
         for (int i = 0; i < internalList.size(); i++) {
             Budget toCheck = internalList.get(i);
@@ -117,18 +100,6 @@ public class UniqueBudgetList implements Iterable<Budget> {
     }
 
     /**
-     * Removes the budget with the specific index from the list.
-     * The budget of the index must exist in the list.
-     * @param index of the budget to be removed.
-     */
-    public void remove(int index) throws IndexOutOfBoundsException {
-        if (index < 0 || index >= internalList.size()) {
-            throw new IndexOutOfBoundsException();
-        }
-        internalList.remove(index, index + 1);
-    }
-
-    /**
      * Replaces the contents of this list with {@code budgetList}.
      * {@code budgetList} must not contain duplicate budgetList.
      * @param newBudgetList to replace.
@@ -144,19 +115,24 @@ public class UniqueBudgetList implements Iterable<Budget> {
     }
 
     /**
-     * Replaces the previous budget in the list with {@code editedBudget}.
-     * The budget identity of {@code editedBudget} must not be the same as another existing budget in the list.
+     * Returns true if {@code budgetList} contains only unique budgetList.
      */
-    public void replaceLatestBudgetWith(Budget editedBudget) {
-        requireAllNonNull(internalList, editedBudget);
-        internalList.set(0, editedBudget);
+    private boolean budgetsAreUnique(List<Budget> budgetList) {
+        for (int i = 0; i < budgetList.size() - 1; i++) {
+            for (int j = i + 1; j < budgetList.size(); j++) {
+                if (budgetList.get(i).equals(budgetList.get(j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     /**
      * Ensures the size of budgetList does not exceed {@code MAXIMUM_SIZE}.
      * Deletes all budgets after the {@code MAXIMUM_SIZE} has exceeded.
      */
-    public void limitSize() {
+    private void limitSize() {
         requireNonNull(internalList);
         int budgetListSize = internalList.size();
         if (budgetListSize > MAXIMUM_SIZE) {
@@ -164,6 +140,15 @@ public class UniqueBudgetList implements Iterable<Budget> {
                 internalList.remove(MAXIMUM_SIZE);
             }
         }
+    }
+
+    /**
+     * Removes the budget with the specific index from the list.
+     * The budget of the index must exist in the list.
+     * @param index of the budget to be removed.
+     */
+    public void remove(int index) {
+        internalList.remove(index, index + 1);
     }
 
     /**
@@ -183,24 +168,5 @@ public class UniqueBudgetList implements Iterable<Budget> {
         return other == this // short circuit if same object
                 || (other instanceof UniqueBudgetList // instanceof handles nulls
                         && internalList.equals(((UniqueBudgetList) other).internalList));
-    }
-
-    @Override
-    public int hashCode() {
-        return internalList.hashCode();
-    }
-
-    /**
-     * Returns true if {@code budgetList} contains only unique budgetList.
-     */
-    private boolean budgetsAreUnique(List<Budget> budgetList) {
-        for (int i = 0; i < budgetList.size() - 1; i++) {
-            for (int j = i + 1; j < budgetList.size(); j++) {
-                if (budgetList.get(i).equals(budgetList.get(j))) {
-                    return false;
-                }
-            }
-        }
-        return true;
     }
 }
--- src/test/java/seedu/address/model/epiggy/UniqueBudgetListTest.java
+++ src/test/java/seedu/address/model/epiggy/UniqueBudgetListTest.java
@@ -1,20 +1,26 @@
 package seedu.address.model.epiggy;
 
 import static org.junit.Assert.assertEquals;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_TWO;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_AMOUNT_FIRSTEXTRA;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_DATE_FIRSTEXTRA;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_PERIOD_FIRSTEXTRA;
+import static seedu.address.testutil.TypicalBudgets.FIRST_EXTRA;
 import static seedu.address.testutil.TypicalBudgets.ONE;
+import static seedu.address.testutil.TypicalBudgets.SECOND_EXTRA;
 import static seedu.address.testutil.TypicalBudgets.TWO;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-import java.util.Optional;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import seedu.address.model.epiggy.exceptions.DuplicateBudgetException;
 import seedu.address.testutil.epiggy.BudgetBuilder;
 
 public class UniqueBudgetListTest {
@@ -27,6 +33,18 @@ public class UniqueBudgetListTest {
     public void addAtIndex_nullBudget_throwsNullPointerException() {
         thrown.expect(NullPointerException.class);
         uniqueBudgetList.addAtIndex(0, null);
+    }
+
+    @Test
+    public void addAtIndex_indexTooBig_throwsIndexOutOfBoundsException() {
+        thrown.expect(IndexOutOfBoundsException.class);
+        uniqueBudgetList.addAtIndex(UniqueBudgetList.MAXIMUM_SIZE + 1, ONE);
+    }
+
+    @Test
+    public void addAtIndex_negativeIndex_throwsIndexOutOfBoundsException() {
+        thrown.expect(IndexOutOfBoundsException.class);
+        uniqueBudgetList.addAtIndex(-1, ONE);
     }
 
     @Test
@@ -53,156 +71,185 @@ public class UniqueBudgetListTest {
         uniqueBudgetList.replaceAtIndex(0, ONE);
         UniqueBudgetList expectedUniqueBudgetList = new UniqueBudgetList();
         expectedUniqueBudgetList.addAtIndex(0, ONE);
-        assertEquals(expectedUniqueBudgetList, uniqueBudgetList);
+        assertEquals(uniqueBudgetList, expectedUniqueBudgetList);
     }
 
     @Test
-    public void contains_nullBudget_throwsNullPointerException() {
-        thrown.expect(NullPointerException.class);
-        uniqueBudgetList.contains(null);
-    }
-
-    @Test
-    public void contains_budgetNotInList_returnsFalse() {
-        assertFalse(uniqueBudgetList.contains(ONE));
-    }
-
-    @Test
-    public void contains_budgetInList_returnsTrue() {
-        uniqueBudgetList.add(ONE);
-        assertTrue(uniqueBudgetList.contains(ONE));
-    }
-
-    @Test
-    public void contains_budgetWithSameIdentityFieldsInList_returnsTrue() {
-        uniqueBudgetList.add(ONE);
-        Budget editedAlice = new BudgetBuilder(ONE).withAddress(VALID_ADDRESS_TWO).withTags(VALID_TAG_HUSBAND)
-                .build();
-        assertTrue(uniqueBudgetList.contains(editedAlice));
-    }
-
-    @Test
-    public void add_nullBudget_throwsNullPointerException() {
-        thrown.expect(NullPointerException.class);
-        uniqueBudgetList.add(null);
-    }
-
-    @Test
-    public void setBudget_nullTargetBudget_throwsNullPointerException() {
-        thrown.expect(NullPointerException.class);
-        uniqueBudgetList.setBudget(null, ONE);
-    }
-
-    @Test
-    public void setBudget_nullEditedBudget_throwsNullPointerException() {
-        thrown.expect(NullPointerException.class);
-        uniqueBudgetList.setBudget(ONE, null);
-    }
-
-    @Test
-    public void setBudget_targetBudgetNotInList_throwsBudgetNotFoundException() {
-        thrown.expect(BudgetNotFoundException.class);
-        uniqueBudgetList.setBudget(ONE, ONE);
-    }
-
-    @Test
-    public void setBudget_editedBudgetIsSameBudget_success() {
-        uniqueBudgetList.add(ONE);
-        uniqueBudgetList.setBudget(ONE, ONE);
+    public void replaceAtIndex_editedBudgetHasSameIdentity_success() {
+        uniqueBudgetList.addAtIndex(0, FIRST_EXTRA);
+        Budget editedOne = new BudgetBuilder().withAmount(VALID_AMOUNT_FIRSTEXTRA).withPeriod(VALID_PERIOD_FIRSTEXTRA)
+                .withDate(VALID_DATE_FIRSTEXTRA).build();
+        uniqueBudgetList.replaceAtIndex(0, editedOne);
         UniqueBudgetList expectedUniqueBudgetList = new UniqueBudgetList();
-        expectedUniqueBudgetList.add(ONE);
-        assertEquals(expectedUniqueBudgetList, uniqueBudgetList);
+        expectedUniqueBudgetList.addAtIndex(0, editedOne);
+        assertEquals(uniqueBudgetList, expectedUniqueBudgetList);
     }
 
     @Test
-    public void setBudget_editedBudgetHasSameIdentity_success() {
-        uniqueBudgetList.add(ONE);
-        Budget editedAlice = new BudgetBuilder(ONE).withAddress(VALID_ADDRESS_TWO).withTags(VALID_TAG_HUSBAND)
-                .build();
-        uniqueBudgetList.setBudget(ONE, editedAlice);
+    public void replaceAtIndex_editedBudgetHasDifferentIdentity_success() {
+        uniqueBudgetList.addAtIndex(0, ONE);
+        uniqueBudgetList.replaceAtIndex(0, TWO);
         UniqueBudgetList expectedUniqueBudgetList = new UniqueBudgetList();
-        expectedUniqueBudgetList.add(editedAlice);
-        assertEquals(expectedUniqueBudgetList, uniqueBudgetList);
+        expectedUniqueBudgetList.addAtIndex(0, TWO);
+        assertEquals(uniqueBudgetList, expectedUniqueBudgetList);
     }
 
     @Test
-    public void setBudget_editedBudgetHasDifferentIdentity_success() {
-        uniqueBudgetList.add(ONE);
-        uniqueBudgetList.setBudget(ONE, TWO);
+    public void getBudgetAtIndex_emptyUniqueBudgetList_throwsNullPointerException() {
+        UniqueBudgetList emptyUniqueBudgetList = new UniqueBudgetList();
+        thrown.expect(IndexOutOfBoundsException.class);
+        emptyUniqueBudgetList.getBudgetAtIndex(0);
+    }
+
+    @Test
+    public void getBudgetAtIndex_indexTooBig_throwsIndexOutOfBoundsException() {
+        thrown.expect(IndexOutOfBoundsException.class);
+        uniqueBudgetList.getBudgetAtIndex(uniqueBudgetList.getBudgetListSize());
+    }
+
+    @Test
+    public void getBudgetAtIndex_negativeIndex_throwsIndexOutOfBoundsException() {
+        thrown.expect(IndexOutOfBoundsException.class);
+        uniqueBudgetList.getBudgetAtIndex(-1);
+    }
+
+    @Test
+    public void getBudgetAtIndex_success() {
+        uniqueBudgetList.addAtIndex(0, ONE);
+        uniqueBudgetList.addAtIndex(1, TWO);
+        assertEquals(ONE, uniqueBudgetList.getBudgetAtIndex(0));
+        assertEquals(TWO, uniqueBudgetList.getBudgetAtIndex(1));
+    }
+
+    @Test
+    public void getBudgetIndexBasedOnDate_emptyUniqueBudgetList_success() {
+        UniqueBudgetList emptyUniqueBudgetList = new UniqueBudgetList();
+        assertEquals(-1, emptyUniqueBudgetList.getBudgetIndexBasedOnDate(new Date()));
+    }
+
+    @Test
+    public void getBudgetIndexBasedOnDate_nullDate_throwsNullPointerException() {
+        uniqueBudgetList.addAtIndex(0, ONE);
+        thrown.expect(NullPointerException.class);
+        uniqueBudgetList.getBudgetIndexBasedOnDate(null);
+    }
+
+    @Test
+    public void getBudgetIndexBasedOnDate_budgetNotFound_success() {
+        uniqueBudgetList.addAtIndex(0, ONE);
+        assertEquals(-1, uniqueBudgetList.getBudgetIndexBasedOnDate(new Date("01/01/2000")));
+    }
+
+    @Test
+    public void getBudgetIndexBasedOnDate_budgetSuccess_success() {
+        uniqueBudgetList.addAtIndex(0, FIRST_EXTRA);
+        assertEquals(0, uniqueBudgetList.getBudgetIndexBasedOnDate(new Date(VALID_DATE_FIRSTEXTRA)));
+    }
+
+    @Test
+    public void getCurrentBudgetIndex_emptyUniqueBudgetList_success() {
+        UniqueBudgetList emptyUniqueBudgetList = new UniqueBudgetList();
+        assertEquals(-1, uniqueBudgetList.getCurrentBudgetIndex());
+    }
+
+    @Test
+    public void getCurrentBudgetIndex_budgetDoesNotExist_success() {
+        uniqueBudgetList.addAtIndex(0, FIRST_EXTRA);
+        assertEquals(-1, uniqueBudgetList.getCurrentBudgetIndex());
+    }
+
+    @Test
+    public void getCurrentBudgetIndex_success() {
+        Budget currentBudget = new BudgetBuilder().withDate(new Date()).build();
+        uniqueBudgetList.addAtIndex(0, currentBudget);
+        assertEquals(0, uniqueBudgetList.getCurrentBudgetIndex());
+    }
+
+    @Test
+    public void getBudgetListSize_emptyUniqueBudgetList_success() {
+        UniqueBudgetList emptyUniqueBudgetList = new UniqueBudgetList();
+        assertEquals(0, emptyUniqueBudgetList.getBudgetListSize());
+    }
+
+    @Test
+    public void getBudgetListSize_success() {
+        uniqueBudgetList.addAtIndex(0, FIRST_EXTRA);
+        uniqueBudgetList.addAtIndex(1, SECOND_EXTRA);
+        assertEquals(2, uniqueBudgetList.getBudgetListSize());
+    }
+
+    @Test
+    public void addBudgetList_replacesOwnListWithProvidedBudgetList_success() {
+        uniqueBudgetList.addAtIndex(0, ONE);
+        List<Budget> expectedBudgetList = new ArrayList<Budget>();
+        expectedBudgetList.add(0, TWO);
         UniqueBudgetList expectedUniqueBudgetList = new UniqueBudgetList();
-        expectedUniqueBudgetList.add(TWO);
+        expectedUniqueBudgetList.addAtIndex(0, TWO);
+        uniqueBudgetList.addBudgetList(expectedBudgetList);
         assertEquals(expectedUniqueBudgetList, uniqueBudgetList);
     }
 
     @Test
-    public void setBudget_editedBudgetHasNonUniqueIdentity_throwsDuplicateBudgetException() {
-        uniqueBudgetList.add(ONE);
-        uniqueBudgetList.add(TWO);
+    public void addBudgetList_replacesOwnListWithDuplicateBudgetList_throwsDuplicateBudgetException() {
+        uniqueBudgetList.addAtIndex(0, ONE);
+        List<Budget> expectedBudgetList = new ArrayList<Budget>();
+        expectedBudgetList.add(0, ONE);
+        expectedBudgetList.add(1, ONE);
         thrown.expect(DuplicateBudgetException.class);
-        uniqueBudgetList.setBudget(ONE, TWO);
+        uniqueBudgetList.addBudgetList(expectedBudgetList);
     }
 
     @Test
-    public void remove_nullBudget_throwsNullPointerException() {
+    public void addBudgetList_nullList_throwsNullPointerException() {
         thrown.expect(NullPointerException.class);
-        uniqueBudgetList.remove(null);
+        uniqueBudgetList.addBudgetList((List<Budget>) null);
     }
 
     @Test
-    public void remove_budgetDoesNotExist_throwsBudgetNotFoundException() {
-        thrown.expect(BudgetNotFoundException.class);
-        uniqueBudgetList.remove(ONE);
+    public void remove_indexTooBig_throwsIndexOutOfBoundsException() {
+        thrown.expect(IndexOutOfBoundsException.class);
+        uniqueBudgetList.remove(uniqueBudgetList.getBudgetListSize());
     }
 
     @Test
-    public void remove_existingBudget_removesBudget() {
-        uniqueBudgetList.add(ONE);
-        uniqueBudgetList.remove(ONE);
+    public void remove_negativeIndex_throwsIndexOutOfBoundsException() {
+        thrown.expect(IndexOutOfBoundsException.class);
+        uniqueBudgetList.remove(-1);
+    }
+
+    @Test
+    public void remove_existingBudget_success() {
+        uniqueBudgetList.addAtIndex(0, ONE);
+        uniqueBudgetList.remove(0);
         UniqueBudgetList expectedUniqueBudgetList = new UniqueBudgetList();
         assertEquals(expectedUniqueBudgetList, uniqueBudgetList);
-    }
-
-    @Test
-    public void setBudgets_nullUniqueBudgetList_throwsNullPointerException() {
-        thrown.expect(NullPointerException.class);
-        uniqueBudgetList.setBudgets((UniqueBudgetList) null);
-    }
-
-    @Test
-    public void setBudgets_uniqueBudgetList_replacesOwnListWithProvidedUniqueBudgetList() {
-        uniqueBudgetList.add(ONE);
-        UniqueBudgetList expectedUniqueBudgetList = new UniqueBudgetList();
-        expectedUniqueBudgetList.add(TWO);
-        uniqueBudgetList.setBudgets(expectedUniqueBudgetList);
-        assertEquals(expectedUniqueBudgetList, uniqueBudgetList);
-    }
-
-    @Test
-    public void setBudgets_nullList_throwsNullPointerException() {
-        thrown.expect(NullPointerException.class);
-        uniqueBudgetList.setBudgets((List<Budget>) null);
-    }
-
-    @Test
-    public void setBudgets_list_replacesOwnListWithProvidedList() {
-        uniqueBudgetList.add(ONE);
-        List<Budget> budgetList = Collections.singletonList(TWO);
-        uniqueBudgetList.setBudgets(budgetList);
-        UniqueBudgetList expectedUniqueBudgetList = new UniqueBudgetList();
-        expectedUniqueBudgetList.add(TWO);
-        assertEquals(expectedUniqueBudgetList, uniqueBudgetList);
-    }
-
-    @Test
-    public void setBudgets_listWithDuplicateBudgets_throwsDuplicateBudgetException() {
-        List<Budget> listWithDuplicateBudgets = Arrays.asList(ONE, ONE);
-        thrown.expect(DuplicateBudgetException.class);
-        uniqueBudgetList.setBudgets(listWithDuplicateBudgets);
     }
 
     @Test
     public void asUnmodifiableObservableList_modifyList_throwsUnsupportedOperationException() {
         thrown.expect(UnsupportedOperationException.class);
         uniqueBudgetList.asUnmodifiableObservableList().remove(0);
+    }
+
+    @Test
+    public void equals() {
+        // same object -> returns true
+        uniqueBudgetList.addAtIndex(0, FIRST_EXTRA);
+        uniqueBudgetList.addAtIndex(1, SECOND_EXTRA);
+        assertTrue(uniqueBudgetList.equals(uniqueBudgetList));
+
+        // same internal list -> returns true
+        UniqueBudgetList copyOfUniqueBudgetList = uniqueBudgetList;
+        assertTrue(copyOfUniqueBudgetList.equals(uniqueBudgetList));
+
+        // different order -> returns false
+        UniqueBudgetList differentUniqueBudgetList = new UniqueBudgetList();
+        differentUniqueBudgetList.addAtIndex(0, SECOND_EXTRA);
+        differentUniqueBudgetList.addAtIndex(1, FIRST_EXTRA);
+        assertFalse(differentUniqueBudgetList.equals(uniqueBudgetList));
+
+        // different budgets -> returns false
+        differentUniqueBudgetList.remove(1);
+        assertFalse(differentUniqueBudgetList.equals(uniqueBudgetList));
     }
 }

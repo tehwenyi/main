454b10b5a59158c2aa46d5ae6b6376fe43f4da36
Kevin <kevinchanxy@gmail.com>
2019-04-06 20:18:27 +0800

    implemented storage for budget

--- src/main/java/seedu/address/storage/epiggy/JsonSerializableEPiggy.java
+++ src/main/java/seedu/address/storage/epiggy/JsonSerializableEPiggy.java
@@ -12,6 +12,7 @@ import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.EPiggy;
 import seedu.address.model.ReadOnlyEPiggy;
 import seedu.address.model.epiggy.Allowance;
+import seedu.address.model.epiggy.Budget;
 import seedu.address.model.epiggy.Expense;
 
 
@@ -21,15 +22,19 @@ import seedu.address.model.epiggy.Expense;
 @JsonRootName(value = "epiggy")
 public class JsonSerializableEPiggy {
     private final List<JsonAdaptedExpense> expenses = new ArrayList<>();
+    private final List<JsonAdaptedBudget> budgets = new ArrayList<>();
     private final JsonAdaptedGoal goal;
+
 
     /**
      * Constructs a {@code JsonSerializableEPiggy} with the given expense.
      */
     @JsonCreator
     public JsonSerializableEPiggy(@JsonProperty("expenses") List<JsonAdaptedExpense> expenses,
-                                  @JsonProperty("goal") JsonAdaptedGoal goal) {
+                                  @JsonProperty("goal") JsonAdaptedGoal goal,
+                                  @JsonProperty("budgets") List<JsonAdaptedBudget> budgets) {
         this.expenses.addAll(expenses);
+        this.budgets.addAll(budgets);
         this.goal = goal;
     }
 
@@ -40,6 +45,7 @@ public class JsonSerializableEPiggy {
      */
     public JsonSerializableEPiggy(ReadOnlyEPiggy source) {
         expenses.addAll(source.getExpenseList().stream().map(JsonAdaptedExpense::new).collect(Collectors.toList()));
+        budgets.addAll(source.getBudgetList().stream().map(JsonAdaptedBudget::new).collect(Collectors.toList()));
         goal = new JsonAdaptedGoal(source.getGoal().get());
     }
 
@@ -57,6 +63,10 @@ public class JsonSerializableEPiggy {
             } else {
                 ePiggy.addExpense(expense);
             }
+        }
+        for (int i = 0; i < budgets.size(); i++) {
+            Budget budget = budgets.get(i).toModelType();
+            ePiggy.addBudget(i, budget);
         }
         ePiggy.setGoal(goal.toModelType());
         return ePiggy;
